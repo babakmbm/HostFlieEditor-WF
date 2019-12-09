842167db129332e91e05b9ac7e1e2a10366fa9d6
babakmbm <babak.h.rahi@gmail.com>
2019-12-09 23:21:56 +0000

    files

---
+++ HostFlieEditor-WF/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace HostFlieEditor_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OutputBox = new System.Windows.Forms.TextBox();
+            this.ShowTextButton = new System.Windows.Forms.Button();
+            this.AddUrlButton = new System.Windows.Forms.Button();
+            this.UrlBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OutputBox
+            // 
+            this.OutputBox.Location = new System.Drawing.Point(0, 330);
+            this.OutputBox.Multiline = true;
+            this.OutputBox.Name = "OutputBox";
+            this.OutputBox.Size = new System.Drawing.Size(800, 117);
+            this.OutputBox.TabIndex = 1;
+            // 
+            // ShowTextButton
+            // 
+            this.ShowTextButton.Location = new System.Drawing.Point(521, 40);
+            this.ShowTextButton.Name = "ShowTextButton";
+            this.ShowTextButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ShowTextButton.Size = new System.Drawing.Size(219, 41);
+            this.ShowTextButton.TabIndex = 2;
+            this.ShowTextButton.Text = "Show Text";
+            this.ShowTextButton.UseVisualStyleBackColor = true;
+            this.ShowTextButton.Click += new System.EventHandler(this.ShowTextButton_Click);
+            // 
+            // AddUrlButton
+            // 
+            this.AddUrlButton.Location = new System.Drawing.Point(521, 114);
+            this.AddUrlButton.Name = "AddUrlButton";
+            this.AddUrlButton.Size = new System.Drawing.Size(219, 40);
+            this.AddUrlButton.TabIndex = 3;
+            this.AddUrlButton.Text = "ADD URL";
+            this.AddUrlButton.UseVisualStyleBackColor = true;
+            this.AddUrlButton.Click += new System.EventHandler(this.AddUrlButton_Click);
+            // 
+            // UrlBox
+            // 
+            this.UrlBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UrlBox.Location = new System.Drawing.Point(12, 114);
+            this.UrlBox.Name = "UrlBox";
+            this.UrlBox.Size = new System.Drawing.Size(474, 36);
+            this.UrlBox.TabIndex = 4;
+            this.UrlBox.TextChanged += new System.EventHandler(this.UrlBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UrlBox);
+            this.Controls.Add(this.AddUrlButton);
+            this.Controls.Add(this.ShowTextButton);
+            this.Controls.Add(this.OutputBox);
+            this.Name = "Form1";
+            this.Text = "Block Website";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox OutputBox;
+        private System.Windows.Forms.Button ShowTextButton;
+        private System.Windows.Forms.Button AddUrlButton;
+        private System.Windows.Forms.TextBox UrlBox;
+    }
+}
+
---
+++ HostFlieEditor-WF/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HostFlieEditor_WF
+{
+    public partial class Form1 : Form
+    {
+        public string path = @"C:\Windows\System32\drivers\etc\hosts";
+        public string[] lines;
+        bool Exist = true;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            if (!File.Exists(path))
+            {
+                OutputBox.Text = "File Missing! \n";
+                OutputBox.ForeColor = System.Drawing.Color.Red;
+                Exist = false;
+            }
+            else
+            {
+                OutputBox.ForeColor = System.Drawing.Color.Black;
+                lines = File.ReadAllLines(path);
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        OutputBox.AppendText("\n" + s + "\n");
+                    }
+                    sr.Close();
+                }
+
+            }
+        }
+
+
+        private void ShowTextButton_Click(object sender, EventArgs e)
+        {
+            if (Exist)
+            {
+                
+            }
+            else
+            {
+                OutputBox.Text = "File does not exist!!!";
+            }
+        }
+
+        private void UrlBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void AddUrlButton_Click(object sender, EventArgs e)
+        {
+            if (UrlBox.Text != "")
+            {
+                System.Net.IPAddress[] addresses = System.Net.Dns.GetHostAddresses(UrlBox.Text);
+                string url = "\n" + addresses[1] + " " + UrlBox.Text + "\n";
+                OutputBox.AppendText(url);
+                using (StreamWriter sr = File.AppendText(path))
+                {
+                    sr.WriteLine(url);
+                    sr.Close();
+                }
+            }
+            else
+            {
+                OutputBox.AppendText("\n" + "Please enter a url to block" + "\n");
+            }
+            
+        }
+    }
+}
